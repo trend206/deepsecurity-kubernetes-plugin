b2fdd7bd44aa1562750fc627b4d68ba5e20b458b
Jeff Thorne <jeff.thorne@gmail.com>
2017-11-21 12:54:08 -0800

    added initial create connector

--- ds.py
+++ ds.py
@@ -6,36 +6,31 @@ import urllib
 from tabulate import tabulate
 import click
 
-from utils import *
+from utils import Utils
 from models import KubeNode, Node
 
 
-dsm = get_ds_api_object(yaml.safe_load(open('config.yaml', 'r')), kube_interface.api)
+
 
 @click.group()
 def cli():
     pass
 
 @cli.command()
-@click.option('/create', default=False, help='creates a Kubernetes connector in DSM.')
-@click.option('/clear', is_flag=True, help="removes all k8s nodes from connectors")
-#@click.option('--exists', is_flag=True, help="checks to see if any Kubernetes connectors exist")
-def connector(create, clear):
-    dsm = get_ds_api_object(yaml.safe_load(open('config.yaml', 'r')), kube_interface.api)
-    if clear:
-        ids = dshosts_that_map_to_kubenodes(dsm)
-        dsm.host_move_to_hosts_group(ids, 0)
-    elif create:
-        print(create_connector(dsm, create))
+@click.argument('name', default="my-k8s-cluster")
+def create_connector(name):
+    utils = Utils()
+    print(utils.create_connector(name))
+    utils.end_session()
 
-    dsm.end_session()
 
 @cli.command()
 def nodes():
     try:
         print()
-        kube_node_objs = kube_interface.get_nodes()
-        ds_hosts = dsm.host_retrieve_all()
+        utils = Utils()
+        kube_node_objs = utils.kube_interface.get_nodes()
+        ds_hosts = utils.dsm.host_retrieve_all()
 
         node_objs = []
         node_info_objs = []
@@ -48,7 +43,7 @@ def nodes():
                 exists = [x for x in ds_hosts if x['displayName'] == address]
 
                 if exists and exists[0]:
-                    ds_host_detail = get_host_details(dsm, exists[0]['ID'])
+                    ds_host_detail = utils.get_host_details(exists[0]['ID'])
                     if ds_host_detail:
                         node_obj = Node(node_info[0], node_info[1], node_info[2], node_info[3], node_info[4], ds_host_detail['Agent Status'],
                                         ds_host_detail['AM'], ds_host_detail['WR'], ds_host_detail['DPI'], ds_host_detail['FW'], ds_host_detail['IM'], ds_host_detail['LI'], ds_host_detail['TUM'])
@@ -61,7 +56,7 @@ def nodes():
                 node_info = node_info + add_details
 
             node_info_objs.append(node_info)
-        dsm.end_session()
+        utils.end_session()
         print(tabulate(node_info_objs, headers=['NAME', 'STATUS', 'ROLE', 'AGE', 'VERSION', 'Operating System','Container Runtime','DS AGENT STATUS', 'AM', 'WR','DPI', 'FW', 'IM', 'LI', 'TUM']), end='\n\n')
 
     except urllib.error.URLError as urle:
@@ -70,7 +65,7 @@ def nodes():
     except Exception as e:
         print("--------------------------------------------------")
         print(type(e))
-        dsm.end_session()
+
 
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ import sys
 import base64
 import json
 import pytz
+import yaml
 from datetime import datetime
 from dsp3.models.manager import Manager
 from kubernetes import client, config
@@ -12,97 +13,105 @@ from models import KubeNode, Node
 
 kube_interface = KubeInterface()
 
+class Utils():
 
-def get_host_details(dsm, host_id):
-    status = {}
-    host = dsm.host_status(host_id)
+    def __init__(self):
+        self.kube_interface = KubeInterface()
+        self.dsm = self.get_ds_api_object(yaml.safe_load(open('config.yaml', 'r')))
 
-    status['Agent Status'] = host['overallStatus']
-    status['AM'] = "Off" if "Off" in host['overallAntiMalwareStatus'] else "On"
-    status['WR'] = "Off" if "Off" in host['overallWebReputationStatus'] else "On"
-    status['DPI'] = "Off" if "Off" in host['overallDpiStatus'] else "On"
-    status['FW'] = "Off" if "Off" in host['overallFirewallStatus'] else "On"
-    status['IM'] = "Off" if "Off" in host['overallIntegrityMonitoringStatus'] else "On"
-    status['LI'] = "Off" if "Off" in host['overallLogInspectionStatus'] else "On"
-    trusted_update_mode = json.loads(dsm.get_trusted_update_mode(host_id))
-    status['TUM'] = "Off" if "off" in trusted_update_mode['DescribeTrustedUpdateModeResponse']['state'] else "On"
-    return status
+    def get_host_details(self, host_id):
+        status = {}
+        host = self.dsm.host_status(host_id)
 
-
-def calculate_age(born):
-    age = (datetime.now().replace(tzinfo=pytz.utc) - born).days
-    return 1 if age == 0 else age
-
-def get_node_role(labels):
-    role = "<none>"
-    for label in labels:
-        if 'node-role.kubernetes.io' in label:
-            role = label.split("/")[1]
-    return role
-
-def get_ds_password(kube_api):
-    secrets = kube_api.list_secret_for_all_namespaces().items
-    password = None
-    for secret in secrets:
-        if secret.metadata.name == 'deepsecurity':
-            password = base64.b64decode(secret.data['password']).decode('utf-8')
-
-    return password
+        status['Agent Status'] = host['overallStatus']
+        status['AM'] = "Off" if "Off" in host['overallAntiMalwareStatus'] else "On"
+        status['WR'] = "Off" if "Off" in host['overallWebReputationStatus'] else "On"
+        status['DPI'] = "Off" if "Off" in host['overallDpiStatus'] else "On"
+        status['FW'] = "Off" if "Off" in host['overallFirewallStatus'] else "On"
+        status['IM'] = "Off" if "Off" in host['overallIntegrityMonitoringStatus'] else "On"
+        status['LI'] = "Off" if "Off" in host['overallLogInspectionStatus'] else "On"
+        trusted_update_mode = json.loads(self.dsm.get_trusted_update_mode(host_id))
+        status['TUM'] = "Off" if "off" in trusted_update_mode['DescribeTrustedUpdateModeResponse']['state'] else "On"
+        return status
 
 
-def get_ds_api_object(config, kube_api):
-    dsm = None
-    using_dsas = config['using_dsas']
-    username = config['ds_username']
-    password = get_ds_password(kube_api)
-    if password == None:
-        print("Deep Security password not found as Kubernetes Secret")
-        sys.exit()
+    def calculate_age(self, born):
+        age = (datetime.now().replace(tzinfo=pytz.utc) - born).days
+        return 1 if age == 0 else age
 
-    host = config['ds_host']
-    port = config['ds_port']
-    if using_dsas:
-        tenant = config['dsas_tenant']
-        dsm = Manager(username=username, password=password, tenant=tenant)
-    else:
-        dsm = Manager(username=username, password=password, host=host, port=port)
+    def get_node_role(self, labels):
+        role = "<none>"
+        for label in labels:
+            if 'node-role.kubernetes.io' in label:
+                role = label.split("/")[1]
+        return role
 
-    return dsm
+    def get_ds_password(self, kube_api):
+        secrets = kube_api.list_secret_for_all_namespaces().items
+        password = None
+        for secret in secrets:
+            if secret.metadata.name == 'deepsecurity':
+                password = base64.b64decode(secret.data['password']).decode('utf-8')
 
-
-def _does_connector_exist(dsm, name):
-    connector_exists = False
-    id = None
-    host_groups = dsm.host_group_retrieve_all()
-    exists = [x for x in host_groups if x['name'] == name]
-    if len(exists) > 0:
-        connector_exists = True
-        id = exists[0]['ID']
-
-    return (connector_exists , id)
-
-def dshosts_that_map_to_kubenodes(dsm):
-    ids = []
-    ds_hosts = dsm.host_retrieve_all()
-    kube_nodes = kube_interface.get_nodes()
-
-    for node in kube_nodes:
-        for address in node.addresses:
-            exists = [x for x in ds_hosts if x['displayName'] == address]
-
-            if exists and exists[0]:
-                ids.append(exists[0]['ID'])
-
-    return ids
+        return password
 
 
-def create_connector(dsm, name):
-    exists, id = _does_connector_exist(dsm, name)
-    if not exists:
-        hg = dsm.host_group_create(name=name)
-        ids = dshosts_that_map_to_kubenodes(dsm)
-        dsm.host_move_to_hosts_group(ids, hg['ID'])
-        return "Kubernetes connector %s created succesfully" % name
-    else:
-        return "Kubernetes connector with that name already exists"
+    def get_ds_api_object(self, config):
+        dsm = None
+        using_dsas = config['using_dsas']
+        username = config['ds_username']
+        password = self.get_ds_password(self.kube_interface.api)
+        if password == None:
+            print("Deep Security password not found as Kubernetes Secret")
+            sys.exit()
+
+        host = config['ds_host']
+        port = config['ds_port']
+        if using_dsas:
+            tenant = config['dsas_tenant']
+            dsm = Manager(username=username, password=password, tenant=tenant)
+        else:
+            dsm = Manager(username=username, password=password, host=host, port=port)
+
+        return dsm
+
+
+    def _does_connector_exist(self, name):
+        connector_exists = False
+        id = None
+        host_groups = self.dsm.host_group_retrieve_all()
+        exists = [x for x in host_groups if x['name'] == name]
+        if len(exists) > 0:
+            connector_exists = True
+            id = exists[0]['ID']
+
+        return (connector_exists , id)
+
+    def dshosts_that_map_to_kubenodes(self):
+        ids = []
+        ds_hosts = self.dsm.host_retrieve_all()
+        kube_nodes = kube_interface.get_nodes()
+
+        for node in kube_nodes:
+            for address in node.addresses:
+                exists = [x for x in ds_hosts if x['displayName'] == address]
+
+                if exists and exists[0]:
+                    ids.append(exists[0]['ID'])
+
+        return ids
+
+
+    def create_connector(self, name):
+        exists, id = self._does_connector_exist(name)
+        if not exists:
+            hg = self.dsm.host_group_create(name=name)
+            ids = self.dshosts_that_map_to_kubenodes()
+            self.dsm.host_move_to_hosts_group(ids, hg['ID'])
+            return "Kubernetes connector %s created succesfully" % name
+        else:
+            return "Kubernetes connector with that name already exists"
+
+    def end_session(self):
+        self.dsm.end_session()
 
